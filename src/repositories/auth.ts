6dcbb42da6391428ea83e19a1ea3fb48ecd0308f
h.toriyama <toriyamahiroto1128@gmail.com>
2023-05-20 15:20:02 +0900

    db追加

--- src/repositories/auth.ts
+++ src/repositories/auth.ts
@@ -3,28 +3,31 @@ import {
   onAuthStateChanged,
   signOut as firebaseSignOut,
 } from 'firebase/auth';
-// import {doc, getDoc, setDoc} from 'firebase/firestore';
+import {doc, getDoc, setDoc, getFirestore} from 'firebase/firestore';
 import {login, logout} from '../redux/slices/authSlice';
 import {AppDispatch} from '../redux/store';
 
 export const monitorAuthState = (dispatch: AppDispatch) => {
+  const db = getFirestore();
+
   onAuthStateChanged(getAuth(), async (user) => {
     if (user) {
-      // const docRef = doc(db, 'users', user.uid);
-      // const docSnap = await getDoc(docRef);
-      // if (docSnap.exists()) {
-      //   dispatch(update(docSnap.data() as User));
-      // } else {
-      //   const newUser = {
-      //     uid: user.uid,
-      //     displayName: user.displayName,
-      //     email: user.email,
-      //     twitterUrl: '',
-      //   };
-      //   await setDoc(docRef, newUser);
-      //   dispatch(login(newUser));
-      // }
-      dispatch(login(user));
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        dispatch(login(user));
+        // dispatch(update(docSnap.data() as User));
+      } else {
+        const newUser = {
+          uid: user.uid,
+          displayName: user.displayName,
+          email: user.email,
+          twitterUrl: '',
+        };
+        await setDoc(docRef, newUser);
+        dispatch(login(user));
+      }
+      // dispatch(login(user));
     } else {
       dispatch(logout());
     }
