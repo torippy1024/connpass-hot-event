b1619cdc6f8213128287ffb73fc11426e430571e
torippy <47879788+torippy1024@users.noreply.github.com>
2023-08-19 15:47:50 +0900

    不要なファイル削除

--- src/models/AuthUser.ts
+++
@@ -1,9 +0,0 @@
-type AuthUser = {
-  uid: string;
-  email: string | null;
-  displayName: string | null;
-  phoneNumber: string | null;
-  photoURL: string | null;
-};
-
-export default AuthUser;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: {},
 });
 
 export type AppDispatch = typeof store.dispatch;
